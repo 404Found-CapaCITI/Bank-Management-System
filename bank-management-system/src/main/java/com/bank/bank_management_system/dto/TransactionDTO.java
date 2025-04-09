e76c8458aa3bfd6363aa21826b097500baea88c1
1632Pelma <125448084+1632Pelma@users.noreply.github.com>
2025-04-09 08:48:02 +0200

    Update TransactionDTO.java

--- bank-management-system/src/main/java/com/bank/bank_management_system/dto/TransactionDTO.java
+++ bank-management-system/src/main/java/com/bank/bank_management_system/dto/TransactionDTO.java
@@ -1,54 +1,17 @@
-
 package com.bank.bank_management_system.dto;
+import com.bank.bank_management_system.repository.*;;
 
+import com.bank.bank_management_system.model.*;
 
+import lombok.Data;
 import java.time.LocalDateTime;
 
-public class TransactionDTO {
+@Data
+public class TransactionDto {
     private Long id;
     private double amount;
-    private String description;
-    private LocalDateTime dateTime;
     private String type;
-
-    // Getters and setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
+    private String description;
+    private LocalDateTime transactionDate;
+    private Long accountId;
 }
