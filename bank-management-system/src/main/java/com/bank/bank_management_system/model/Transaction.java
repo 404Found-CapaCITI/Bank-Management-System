fa28b19acc4203ed8509c138609800af50d08aa3
1632Pelma <125448084+1632Pelma@users.noreply.github.com>
2025-04-09 08:48:19 +0200

    Update Transaction.java

--- bank-management-system/src/main/java/com/bank/bank_management_system/model/Transaction.java
+++ bank-management-system/src/main/java/com/bank/bank_management_system/model/Transaction.java
@@ -1,80 +1,28 @@
 package com.bank.bank_management_system.model;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import java.time.LocalDateTime;
 
+@Data
 @Entity
+@Table(name = "transactions")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     
     private double amount;
+    private String type; // DEPOSIT, WITHDRAWAL, TRANSFER
     private String description;
-    private LocalDateTime dateTime;
-    
-    @Enumerated(EnumType.STRING)
-    private TransactionType type;
+    private LocalDateTime transactionDate;
     
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id")
-    private BankAccount account;
-
-    public Transaction() {}
-
-    public Transaction(double amount, String description, TransactionType type, BankAccount account) {
-        this.amount = amount;
-        this.description = description;
-        this.type = type;
-        this.account = account;
-        this.dateTime = LocalDateTime.now();
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDateTime dateTime) {
-        this.dateTime = dateTime;
-    }
-
-    public TransactionType getType() {
-        return type;
-    }
-
-    public void setType(TransactionType type) {
-        this.type = type;
-    }
-
-    public BankAccount getAccount() {
-        return account;
-    }
-
-    public void setAccount(BankAccount account) {
-        this.account = account;
+    private Account account;
+    
+    @PrePersist
+    public void prePersist() {
+        this.transactionDate = LocalDateTime.now();
     }
 }
