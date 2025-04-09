98680cc8ed8838b13113979bc7d65446fd7d0781
1632Pelma <125448084+1632Pelma@users.noreply.github.com>
2025-04-09 12:46:24 +0200

    Update AccountController.java

--- bank-management-system/src/main/java/com/bank/bank_management_system/controllers/AccountController.java
+++ bank-management-system/src/main/java/com/bank/bank_management_system/controllers/AccountController.java
@@ -1,11 +1,19 @@
 package com.bank.bank_management_system.controllers;
 
-import com.bank.bank_management_system.dto.*;
-import com.bank.bank_management_system.services.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bank.bank_management_system.dto.AccountDto;
+import com.bank.bank_management_system.services.AccountService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/accounts")
