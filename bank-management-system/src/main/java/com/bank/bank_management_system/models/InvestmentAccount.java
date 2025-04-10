bb36840247a056fcb8fcda89be107721f84a516f
1632Pelma <125448084+1632Pelma@users.noreply.github.com>
2025-04-10 12:01:40 +0200

    updated investment account by a dding a discriminator

--- bank-management-system/src/main/java/com/bank/bank_management_system/models/InvestmentAccount.java
+++ bank-management-system/src/main/java/com/bank/bank_management_system/models/InvestmentAccount.java
@@ -6,6 +6,13 @@ import jakarta.persistence.Entity;
 @Entity
 @DiscriminatorValue("INVESTMENT")
 public class InvestmentAccount extends Account {
+    public InvestmentAccount() {}
+
+    public InvestmentAccount(User user, double initialBalance) {
+        this.setUser(user);
+        this.setBalance(initialBalance);
+        this.setAccountNumber(User.generateAccountNumber());
+    }
 
     @Override
     public String getAccountType() {
