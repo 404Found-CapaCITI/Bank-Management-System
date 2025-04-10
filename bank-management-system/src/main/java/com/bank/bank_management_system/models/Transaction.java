248b3d110475ee2981d8363dfff198fa41a730d0
1632Pelma <125448084+1632Pelma@users.noreply.github.com>
2025-04-10 12:03:34 +0200

    updated the methods for transactions

--- bank-management-system/src/main/java/com/bank/bank_management_system/models/Transaction.java
+++ bank-management-system/src/main/java/com/bank/bank_management_system/models/Transaction.java
@@ -2,6 +2,7 @@ package com.bank.bank_management_system.models;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,35 +10,42 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.Data;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
 @Entity
+@Table(name = "transactions")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Transaction {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String type;
+    @Column(name = "transaction_type")
+    private String transactionType;
+
     private double amount;
+
     private String description;
 
+    @Column(name = "transaction_date")
+    private LocalDateTime transactionDate;
+
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "account_id")
+    @JoinColumn(name = "account_id", nullable = false)
     private Account account;
 
-    public Transaction() {
-    }
-
-    public Transaction(String type, double amount, String description, Account account) {
-        this.type = type;
+    public Transaction(String transactionType, double amount, String description, Account account) {
+        this.transactionType = transactionType;
         this.amount = amount;
         this.description = description;
         this.account = account;
-    }
-
-    public LocalDateTime getTransactionDate() {
-        throw new UnsupportedOperationException("Unimplemented method 'getTransactionDate'");
+        this.transactionDate = LocalDateTime.now();
     }
 }
