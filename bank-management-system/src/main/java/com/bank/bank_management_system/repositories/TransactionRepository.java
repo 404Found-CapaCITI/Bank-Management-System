3c844351becb396644cc73bb7802838012524eeb
1632Pelma <125448084+1632Pelma@users.noreply.github.com>
2025-04-10 12:02:44 +0200

    Added a repo for the transactions

--- bank-management-system/src/main/java/com/bank/bank_management_system/repositories/TransactionRepository.java
+++ bank-management-system/src/main/java/com/bank/bank_management_system/repositories/TransactionRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bank.bank_management_system.models.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> findByAccountId(Long accountId);
     List<Transaction> findByAccountIdOrderByTransactionDateDesc(Long accountId);
 }
